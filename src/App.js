63b4cb0c3fa14f8edce9d8a1eeff5f956dc3cbc2
Mr. Jonah <jaitch@gmail.com>
2019-11-18 18:06:08 -0600

    Moms Reg Form

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import ProfileForm from './components/MomProfileForm'
+
+function App() {
+  return (
+    <div className="App">
+      <ProfileForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MomProfileForm.js
@@ -0,0 +1,99 @@
+import React, {useState, useEffect} from 'react';
+import {withFormik, Form, Field} from 'formik';
+import * as Yup from 'yup'
+
+import {Container, Row, Col, FormGroup, Label} from 'reactstrap'
+
+const ProfileForm = ({values, errors, touched, status}) => {
+    const [formData, setFormData] = useState({});
+
+    useEffect(() => {
+        // api call for user data
+        status && setFormData(status)
+    }, [status])
+
+    return (
+        <>
+            <Form>
+                <FormGroup row>
+                    <Label htmlFor="user_name" sm={3}>Name</Label>
+                    <Col sm={9}>
+                        <Field type="text" name="user_name" id="user_name" className="form-control" />
+                        {touched.user_name && errors.user_name ? (<small className="form-text text-danger">{errors.user_name}</small>) : null}
+                    </Col>
+                </FormGroup>
+
+                <FormGroup row>
+                    <Label htmlFor="user_phone_number" sm={3}>Phone</Label>
+                    <Col sm={9}>
+                        <Field type="text" name="user_phone_number" id="user_phone_number" className="form-control" />
+                        {touched.user_phone_number && errors.user_phone_number ? (<small className="form-text text-danger">{errors.user_phone_number}</small>) : null}
+                    </Col>
+                </FormGroup>
+
+                <FormGroup row>
+                    <Label htmlFor="user_email" sm={3}>Email</Label>
+                    <Col sm={9}>
+                        <Field type="text" name="user_email" id="user_email" className="form-control" />
+                        {touched.user_email && errors.user_email ? (<small className="form-text text-danger">{errors.user_email}</small>) : null}
+                    </Col>
+                </FormGroup>
+
+                <FormGroup row>
+                    <Label htmlFor="user_plot" sm={3}>Address</Label>
+                    <Col sm={9}>
+                        <Field type="text" name="user_plot" id="user_plot" className="form-control" />
+                        {touched.user_plot && errors.user_plot ? (<small className="form-text text-danger">{errors.user_plot}</small>) : null}
+                    </Col>
+                </FormGroup>
+
+                <FormGroup row>
+                    <Label htmlFor="user_password" sm={3}>Password</Label>
+                    <Col sm={9}>
+                        <Field type="password" name="password" id="user_password" className="form-control" />
+                        {touched.password && errors.password ? (<small className="form-text text-danger">{errors.password}</small>) : null}
+                    </Col>
+                </FormGroup>
+
+                <FormGroup row>
+                    <Label htmlFor="due_date" sm={3}>Due Date</Label>
+                    <Col sm={9}>
+                        <Field type="text" name="due_date" id="due_date" className="form-control" />
+                        {touched.due_date && errors.due_date ? (<small className="form-text text-danger">{errors.due_date}</small>) : null}
+                    </Col>
+                </FormGroup>
+                <Field type="hidden" name="user_id" id="user_id" />
+                <button type="submit" className="btn btn-primary">Submit</button>
+                
+            </Form>
+            <p>{`Email Address : ${formData.user_email}`}</p>
+        </>
+    )
+}
+
+const MomProfileForm = withFormik({
+    mapPropsToValues: values => {
+        return {
+            user_id: values.user_id || "",
+            user_name: values.user_name || "",
+            user_email: values.user_email || "",
+            user_phone_number: values.user_phone_number || "",
+            user_plot: values.user_plot || "",
+            password: values.password || "",
+            due_date: values.due_date || ""
+        }
+    },
+    validationSchema: Yup.object().shape({
+        user_name: Yup.string().required('Please, tell us your name.'),
+        user_email: Yup.string().required('You\'ll use your email address as your login.').email('Check you email address'),
+        user_phone_number: Yup.string().required('Please enter a valid phone number'),
+        user_plot: Yup.number(),
+        password: Yup.string().required()
+    }),
+    handleSubmit(values, {resetForm, setStatus}) {
+        setStatus(values);
+        resetForm();
+    }
+})(ProfileForm)
+
+export default MomProfileForm;
